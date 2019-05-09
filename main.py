7716ca5b9fca3c69e67e0a5636822ae6a67ce934
xuweilu <xuweilu07377@hellobike.com>
2019-05-09 14:22:52 +0800

    add pipenv

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import characters
 import metaphysic
 import random
 
+
 def main():
     print("main")
     wuxingCharacters = set(metaphysic.main())
@@ -13,20 +14,25 @@ def main():
     print("Characters got by your wish: ")
     print(desiredCharacters)
 
-    gender = input("Male or female? Input 1 to represent male, input 2 to represent female: ")
+    gender = input(
+        "Male or female? Input 1 to represent male, input 2 to represent female: "
+    )
     if gender == 1:
         genderCharacters = set(characters.male)
     else:
-        genderCharacters =set(characters.female)
+        genderCharacters = set(characters.female)
     print("Characters got by gender: ")
     print(genderCharacters)
     finalResult = desiredCharacters & desiredCharacters & genderCharacters
     print('Final result: ')
     print(finalResult)
     finalResultList = list(finalResult)
-    finalName = finalResultList[random.randint(0, len(finalResultList))] + finalResultList[random.randint(0, len(finalResultList))]
+    finalName = finalResultList[random.randint(
+        0, len(finalResultList))] + finalResultList[random.randint(
+            0, len(finalResultList))]
     print('Test name: ')
     print(finalName)
+
 
 if __name__ == '__main__':
     main()
